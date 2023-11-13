d10e81bf3c8f557246d3587cc095102f1cc097ec
Abhishek suman <abhisheksumancode@gmail.com>
2023-11-13 17:52:03 +0530

    hooks enabled

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,27 +1,52 @@
 import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
+import { restaurantList } from "../content";
+import "./Hero.css";
+
+function filterData(searchTxt, restaurantList) {
+  const filterData = restaurantList.filter((restaurant) =>
+    restaurant.data.name.includes(searchTxt)
+  );
+  return filterData;
+}
+
 const Hero = () => {
   const [SearchTxt, setSearchTxt] = useState("");
-
+  const [restaurantData, setRestaurantData] = useState(restaurantList);
 
   return (
-    <div className="Hero">
-      <div className="search-container">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search"
-          value={SearchTxt}
-          onChange={(e) => {
-            setSearchTxt(e.target.value);
-          }}
-        />
-       
-        <button>Search</button>
+    <>
+      <div className="Hero">
+        <div className="search-container">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search"
+            value={SearchTxt}
+            onChange={(e) => {
+              setSearchTxt(e.target.value);
+            }}
+          />
+
+          <button
+            onClick={() => {
+              const data = filterData(SearchTxt, restaurantList);
+              setRestaurantData(data);
+            }}
+          >
+            Search
+          </button>
+        </div>
       </div>
 
-      <RestaurantCard />
-    </div>
+      <div className="restaurant-list">
+        {restaurantData.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
---
+++ src/Components/content.jsx
@@ -0,0 +1,41 @@
+export const restaurantList = [
+  {
+    type: "restaurant",
+    data: {
+      id: "1",
+      name: "KFC",
+      cuisines: ["American", "Burger"],
+      rating: "4.2",
+    },
+  },
+
+  {
+    type: "restaurant",
+    data: {
+      id: "2",
+      name: "Burger King",
+      cuisines: ["American", "Burger"],
+      rating: "4.0",
+    },
+  },
+
+  {
+    type: "restaurant",
+    data: {
+      id: "3",
+      name: "Xero Degrees",
+      cuisines: ["American", "Burger"],
+      rating: "4.4",
+    },
+  },
+
+  {
+    type: "restaurant",
+    data: {
+      id: "4",
+      name: "Raja Dhaba",
+      cuisines: ["Indian", " Rajma"],
+      rating: "5.0",
+    },
+  },
+];
