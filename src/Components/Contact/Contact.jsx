10297eb67e8502e1ab9f64dc08aaacce2aa8748b
Abhishek suman <abhisheksumancode@gmail.com>
2023-12-22 02:31:02 +0530

    margin updated for project card

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 const Contact = () => {
   return (
-    <div class="flex mx-[10%] gap-4 mt-4">
+    <div class="flex mx-[15%] gap-4 mt-4">
       <div class="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
         <div class="relative mx-4 mt-4 overflow-hidden text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40">
           <img
