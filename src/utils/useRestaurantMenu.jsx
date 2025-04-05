8f48765808cfb82f2c56293f08415666f12703fa
Abhishek suman <abhisheksumancode@gmail.com>
2025-04-05 21:50:33 +0530

    Update useRestaurantMenu.jsx

--- src/utils/useRestaurantMenu.jsx
+++ src/utils/useRestaurantMenu.jsx
@@ -11,7 +11,7 @@ const useResturentMenu=(resId)=>{
 
   let fetchData= async()=>{
 
-    let data=await fetch("https://swiggyapi.azurewebsites.net/restaurant/"+resId)
+    let data=await fetch("http://51.79.250.6:9999/restaurant/"+resId)
     let res=await data.json()
     setResInfo(res)
     
