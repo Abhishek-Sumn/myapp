a9b0f139366a4d413c406671ec0eaec48691eddd
Abhishek suman <abhisheksumancode@gmail.com>
2025-04-05 21:47:47 +0530

    Update content.jsx

--- src/Components/content.jsx
+++ src/Components/content.jsx
@@ -1,9 +1,9 @@
 /*  export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6089845&lng=77.29420809999999&restaurantId="; */
-export const MENU_API="https://swiggyapi.azurewebsites.net/restaurant"; 
+export const MENU_API="http://51.79.250.6:9999/restaurant"; 
 
 export const IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 export const IMG_MENU_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_254,h_160,c_fill/";
 
 export const IMG_OFFER_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_600,c_fill/";
-export const BEST_OFFER_URL = "https://swiggyapi.azurewebsites.net/restaurant";
+export const BEST_OFFER_URL = "http://51.79.250.6:9999/restaurant";
 
